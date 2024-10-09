15060e1dc39bdabe43089f44983b93d61e174986
WeiweiHung <112917097+WeiweiHung@users.noreply.github.com>
2024-10-09 18:26:17 +0800

    U

--- Platformer/Assets/PlayerController.cs
+++ Platformer/Assets/PlayerController.cs
@@ -1,34 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    private Animator anim;
+    private enum State{idle,running,jumping};
+    private State state = State.idle;
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
   
     // Update is called once per frame
     void Update()
     {
-        //left¡Bright movement
-       if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        float hDirection = Input.GetAxis("Horizontal");
+
+        //leftï¿½Bright movement
+       if (hDirection < 0) 
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
         }
-       else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+       else if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
         }
         else
         {
-            rb.velocity = new Vector2(0, rb.velocity.y); 
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
        //jump
         if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.UpArrow))
         {
             rb.velocity = new Vector2(rb.velocity.x, 5);
+            state = State.jumping;
+        }
+        VelocityState();
+        anim.SetInteger("state",(int)state);
+    }
+
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+
+        }
+        else if(Math.Abs(rb.velocity.x) > 2f)
+        {
+            //is running
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
         }
     }
 }
--- UDEMY/Complete_C_Unity_Game_Developer/Delivery Driver/Assets/Delivery.cs
+++ UDEMY/Complete_C_Unity_Game_Developer/Delivery Driver/Assets/Delivery.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+    public float PackageDispearTime = 1;
     // defaul of bool is false 
     void OnCollisionEnter2D(Collision2D other)
      {
@@ -13,13 +15,14 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log(other.tag);
-        if(other.CompareTag("Package"))
+        //Debug.Log(other.tag);
+        if(other.CompareTag("Package") && hasPackage == false)
         {
             
             Debug.Log("Package pick up");
             hasPackage = true;
             Debug.Log(hasPackage);
+            Destroy(other.gameObject,PackageDispearTime);
         }
         else if(other.CompareTag("Customer") && hasPackage)
         {
